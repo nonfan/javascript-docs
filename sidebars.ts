216560ccc0e101614ee4bef23e46b28b5903373d
agoodbook <beher0@foxmail.com>
2024-01-31 06:26:48 +0800

    初始化模板

--- sidebars.ts
+++ sidebars.ts
@@ -1,7 +1,22 @@
 import type { SidebarsConfig } from "@docusaurus/plugin-content-docs";
 
 const sidebars: SidebarsConfig = {
-  docs: ["index"],
+  docs: [
+    {
+      type: "doc",
+      label: "起步",
+      id: "index",
+    },
+    {
+      type: "category",
+      label: "期约与异步函数",
+      items: [
+        "promise-and-async-function/async-program",
+        "promise-and-async-function/promise",
+        "promise-and-async-function/async-function",
+      ],
+    },
+  ],
 };
 
 export default sidebars;
