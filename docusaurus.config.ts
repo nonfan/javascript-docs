1de5ff37a872040cc9351af22e80cad3dd8983ae
agoodbook <152195027+agoodbook@users.noreply.github.com>
2024-01-30 20:16:39 +0800

    初始化模板

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -2,10 +2,11 @@ import { themes as prismThemes } from "prism-react-renderer";
 import type { Config } from "@docusaurus/types";
 import type * as Preset from "@docusaurus/preset-classic";
 const username: string = "agoodbook";
-const repo: string = "agoodbook.github.io";
-let config: Config;
-config = {
-  title: "AGoodBook", // 站点名称
+const repo: string = "javascript";
+const title: string = "JavaScript";
+
+const config: Config = {
+  title: title, // 站点名称
   tagline: "书山有路勤为径，学海无涯苦作舟", // slogan，标语
   favicon: "img/favicon.ico",
   trailingSlash: true,
@@ -13,7 +14,7 @@ config = {
   // 配置网站站点url
   url: `https://agoodbook.github.io/`,
   // 仅agoodbook.github.io仓库使用 '/', 其它仓库页面一律使用 '/<projectName>/'
-  baseUrl: "/",
+  baseUrl: `/${repo}`,
 
   // GitHub部署配置
   organizationName: username, // 用户名
@@ -23,7 +24,6 @@ config = {
   onBrokenLinks: "throw",
   onBrokenMarkdownLinks: "warn",
 
-  plugins: ["docusaurus-plugin-sass"],
   i18n: {
     defaultLocale: "zh-CN",
     locales: ["en", "zh-CN"],
@@ -45,32 +45,12 @@ config = {
       "classic",
       {
         docs: {
+          routeBasePath: "/",
           sidebarPath: "./sidebars.ts",
           editUrl: `https://github.com/${username}/${repo}/tree/main`,
-          lastVersion: "current",
           remarkPlugins: [
             [require("@docusaurus/remark-plugin-npm2yarn"), { sync: true }],
           ],
-          versions: {
-            current: {
-              label: "5.x",
-            },
-            "4.0.0": {
-              label: "4.0.0",
-              path: "4.0.0",
-              banner: "none",
-            },
-            "3.0.0": {
-              label: "3.0.0",
-              path: "3.0.0",
-              banner: "none",
-            },
-            "2.0.0": {
-              label: "2.0.0",
-              path: "2.0.0",
-              banner: "none",
-            },
-          },
         },
         theme: {
           customCss: "./src/css/custom.css",
@@ -85,13 +65,6 @@ config = {
         autoCollapseCategories: true,
       },
     },
-    versions: {
-      "2.0.0": "versioned_docs/version-2.0.0",
-      "3.0.0": "versioned_docs/version-3.0.0",
-      "4.0.0": "versioned_docs/version-4.0.0",
-      "5.0.0": "docs",
-      // 添加更多版本...
-    },
     themes: ["@docusaurus/theme-search-algolia"],
     algolia: {
       appId: "X1Z85QJPUV",
@@ -101,59 +74,16 @@ config = {
     // Replace with your project's social card
     image: "img/docusaurus-social-card.jpg",
     navbar: {
-      title: "AGoodBook", // 导航上站点名称
+      title: title, // 导航上站点名称
       logo: {
-        alt: "AGoodBook", //  站点 logo 文字替换
+        alt: title, //  站点 logo 文字替换
         src: "img/logo.svg", //   站点 logo  链接
       },
       items: [
         {
-          type: "doc",
-          position: "left",
-          docId: "started",
-          label: "文档",
-        },
-        { to: "docs/frontend", label: "前端", position: "left" },
-        { to: "docs/backend", label: "后端", position: "left" },
-        { to: "docs/full-stack", label: "全栈", position: "left" },
-        {
           type: "search",
           position: "left",
           className: "custom-search",
-        },
-        {
-          type: "html",
-          position: "right",
-          value: `
-          <button>Give Feedback</button>
-          <form class="send-container">
-            <div class="email">
-              <input id="feedback-email" type="email" placeholder="您的邮箱"/>
-            </div>
-            <div class="content">
-              <textarea id="feedback-content" placeholder="您反馈什么?"></textarea>
-            </div>
-            <div class="tips"></div>
-            <footer>
-              <div class="emoji-container">
-                <img src="/icons/happy.svg" data-emoji="happy" width="24" height="24" alt="happy"/>
-                <img src="/icons/normal.svg" data-emoji="normal" width="24" height="24" alt="normal"/>
-                <img src="/icons/sad.svg" data-emoji="sad" width="24" height="24" alt="sad"/>
-                <img src="/icons/angry.svg" data-emoji="angry" width="24" height="24" alt="angry"/>
-              </div>
-              <button class="send-btn" onclick="event.preventDefault()">发送</button>
-            </footer>
-          </form>
-          `,
-          className: "custom-feedback",
-        },
-        //   right
-        { to: "docs/up-projects", label: "创意工坊", position: "right" },
-        { to: "docs/storeroom", label: "杂物库", position: "right" },
-
-        {
-          type: "docsVersionDropdown",
-          position: "right",
         },
         {
           type: "localeDropdown",
--- sidebars.ts
+++ sidebars.ts
@@ -1,35 +1,7 @@
 import type { SidebarsConfig } from "@docusaurus/plugin-content-docs";
 
 const sidebars: SidebarsConfig = {
-  docs: [
-    {
-      type: "doc",
-      id: "started",
-      label: "介绍",
-    },
-    {
-      type: "doc",
-      id: "guide",
-      label: "指南",
-    },
-    {
-      type: "doc",
-      id: "actions",
-      label: "Actions自动化",
-    },
-    {
-      type: "category",
-      label: "前端插件库",
-      collapsed: false,
-      link: {
-        type: "generated-index", // 生成菜单列表
-      },
-      items: [
-        { id: "plugin-library/nprogress", label: "进度条", type: "doc" },
-        "plugin-library/rich-text-editor",
-      ],
-    },
-  ],
+  docs: ["index"],
 };
 
 export default sidebars;
