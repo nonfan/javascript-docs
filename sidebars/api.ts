effbb0fd4f082e5817fa33e6416491d999572c4e
mofan <blockchain.mofan@gmail.com>
2024-06-25 23:44:42 +0800

    Post: 添加API Object.create()

--- sidebars/api.ts
+++ sidebars/api.ts
@@ -1,7 +1,14 @@
 import type { SidebarsConfig } from '@docusaurus/plugin-content-docs';
 
 const sidebars: SidebarsConfig = {
-  docs: ['api/index', 'api/array'],
+  docs: [
+    'api/global-object',
+    {
+      type: 'category',
+      label: '基本对象',
+      items: ['api/basic-objects/object'],
+    },
+  ],
 };
 
 export default sidebars.docs;
